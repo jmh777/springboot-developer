a413c51c967f96889449539c890ad6c5ad2a8e89
jmh777 <jiangminghan40@gmail.com>
2026-03-12 12:48:02 +0900

    1

--- src/main/java/me/jiangminghan/springdeveloper/TestController.java
+++ src/main/java/me/jiangminghan/springdeveloper/TestController.java
@@ -2,16 +2,22 @@ package me.jiangminghan.springdeveloper;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
 public class TestController {
     @Autowired
-    TestService testService;
+    private TestService testService;
+
     @GetMapping("/test")
     public List<Member> getAllMembers() {
-        List<Member> members = testService.getAllMembers();
-        return members;
+        return testService.getAllMembers();
+    }
+    @PostMapping("/test")
+    public Member createMember(@RequestBody Member member) {
+        return testService.saveMember(member);
     }
 }
