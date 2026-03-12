09fcf0b31bbe6a5a5befd4859cd99c0779bffc0e
2301450구샤오충 <jiusheng886@gmail.com>
2026-03-12 11:56:24 +0900

    3章内容

---
+++ src/main/java/me/scpark/springdeveloper/TestController.java
@@ -0,0 +1,18 @@
+package me.scpark.springdeveloper;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TestController {
+    @Autowired
+    private  TestService testService;
+
+    @GetMapping("/test")
+    public List<Member> getAllMembers(){
+        return testService.getAllMembers();
+    }
+}
